61d33ee8da3e9847a0103195e7a011a886e40e40
Fenghua Yu <fenghua.yu@intel.com>
2022-09-20 20:50:47 -0700

    vfio: idxd: Change vdev name to contain IDXD device ID
    Current vdev name is vdev<X> where X is a global IDA number. It's hard
    to tell which IDXD device this vdev belongs to from its name.
    
    To have a more meaningful vdev name, change vdev name to vdev<N>.<M>
    where N is IDXD device ID and M is an assigned IDA number.
    
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>

--- drivers/vfio/pci/intel/idxd/vfio_device.c
+++ drivers/vfio/pci/intel/idxd/vfio_device.c
@@ -22,13 +22,6 @@ enum {
 	IDXD_VDEV_TYPE_MAX
 };
 
-/*
- * Since vdev file names are global in DSA devices, define their ida's as
- * global to avoid conflict vdev file names.
- */
-static DEFINE_IDA(vdev_ida);
-static DEFINE_MUTEX(vdev_ida_lock);
-
 static int idxd_vdcm_open(struct vfio_device *vdev)
 {
 	struct vdcm_idxd *vidxd = vdev_to_vidxd(vdev);
@@ -2308,9 +2301,7 @@ static int vdev_device_create(struct idxd_device *idxd, u32 type)
 	dev->bus = &dsa_bus_type;
 	dev->type = &idxd_vdev_device_type;
 
-	mutex_lock(&vdev_ida_lock);
 	parent->id = ida_alloc(&idxd->vdev_ida, GFP_KERNEL);
-	mutex_unlock(&vdev_ida_lock);
 	sprintf(vdev_name, "vdev%u.%u", idxd->id, parent->id);
 	dev_found = device_find_child_by_name(dev->parent, vdev_name);
 	if (dev_found) {
@@ -2348,9 +2339,7 @@ static int vdev_device_remove(struct idxd_device *idxd, char *vdev_name)
 		if (!strcmp(dev_name(dev), vdev_name)) {
 			list_del(&pos->list);
 			device_unregister(dev);
-			mutex_lock(&vdev_ida_lock);
 			ida_free(&idxd->vdev_ida, pos->id);
-			mutex_unlock(&vdev_ida_lock);
 
 			return 0;
 		}
