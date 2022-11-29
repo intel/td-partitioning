73e2d00acfce05ac81aa97ea0dfc6aa1f5006037
Wei Wang <wei.w.wang@intel.com>
2022-11-29 08:38:17 +0800

    KVM: TDX: add seamcall TDH_EXPORT_PAUSE
    The TDH_EXPORT_PAUSE seamcall starts the migration downtime period on
    the source, where the TD is paused.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/vmx/tdx_ops.h
+++ arch/x86/kvm/vmx/tdx_ops.h
@@ -488,6 +488,11 @@ static inline u64 tdh_import_end(hpa_t tdr)
 	return kvm_seamcall(TDH_IMPORT_END, tdr, 0, 0, 0, 0, 0, 0, 0, NULL);
 }
 
+static inline u64 tdh_export_pasue(hpa_t tdr)
+{
+	return kvm_seamcall(TDH_EXPORT_PAUSE, tdr, 0, 0, 0, 0, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */
