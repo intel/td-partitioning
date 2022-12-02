e2158fd15625b4786d3236ce819daadf128bc22f
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2022-12-02 23:32:29 +0000

    virt: tdx-guest: Add Quote generation support
    In TDX guest, the second stage in attestation process is to send the
    TDREPORT to QE/QGS to generate the TD Quote. For platforms that does
    not support communication channels like vsock or TCP/IP, implement
    support to get TD Quote using hypercall. GetQuote hypercall can be used
    by the TD guest to request VMM facilitate the Quote generation via
    QE/QGS. More details about GetQuote hypercall can be found in TDX
    Guest-Host Communication Interface (GHCI) for Intel TDX 1.0, section
    titled "TDG.VP.VMCALL<GetQuote>".
    
    Since GetQuote is an asynchronous request hypercall, it will not block
    till the TD Quote is generated. So VMM uses callback interrupt vector
    configured by SetupEventNotifyInterrupt hypercall to notify the guest
    about Quote generation completion or failure.
    
    GetQuote TDVMCALL requires TD guest pass a 4K aligned shared buffer
    with TDREPORT data as input, which is further used by the VMM to copy
    the TD Quote result after successful Quote generation. To create the
    shared buffer without breaking the direct map, use DMA alloc APIs.
    
    Also note that, shared buffer allocation is currently handled in IOCTL
    handler, although it will increase the TDX_CMD_GET_QUOTE IOCTL response
    time, it is negligible compared to the time required for the quote
    generation completion. So IOCTL performance optimization is not
    considered at this time.
    
    Add support for TDX_CMD_GET_QUOTE IOCTL to allow attestation agent
    submit GetQuote requests from the user space. Since Quote generation
    is an asynchronous request, IOCTL will block indefinitely for the VMM
    response in wait_for_completion_interruptible() call. Using this call
    will also add an option for the user to end the current request
    prematurely by raising any signals. This can be used by attestation
    agent to implement Quote generation timeout feature. If attestation
    agent is aware of time it can validly wait for QE/QGS response, then
    a possible timeout support can be implemented in the user application
    using signals. Quote generation timeout feature is currently not
    implemented in the driver because the current TDX specification does
    not have any recommendation for it.
    
    After submitting the GetQuote request using hypercall, the shared buffer
    allocated for the current request is owned by the VMM. So, during this
    wait window, if the user terminates the request by raising a signal or
    by terminating the application, add a logic to do the memory cleanup
    after receiving the VMM response at a later time.
    
    To support parallel GetQuote requests, use linked list to track the
    active GetQuote requests and upon receiving the callback IRQ, loop
    through the active requests and mark the processed requests complete.
    Users can open multiple instances of the attestation device and send
    GetQuote requests in parallel.
    
    Since GetQuote supports requires usage of DMA APIs and event notify
    interrupt handling, convert TDX guest driver to a platform driver.
    
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -61,6 +61,8 @@ u64 tdx_mcall_verify_report(u8 *reportmac);
 
 int tdx_mcall_extend_rtmr(u8 *data, u8 index);
 
+int tdx_hcall_get_quote(void *tdquote, int size);
+
 bool tdx_allowed_port(short int port);
 
 #else
