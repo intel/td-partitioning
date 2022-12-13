17c067686a309896d7b0582e359caf949c6b6759
donsheng <dongsheng.x.zhang@intel.com>
2022-12-12 17:03:54 -0800

    KVM: TD_PART: Add two functions to check if it is TD Partitioning vcpu/kvm
    Signed-off-by: donsheng <dongsheng.x.zhang@intel.com>

--- arch/x86/kvm/vmx/td_part.h
+++ arch/x86/kvm/vmx/td_part.h
@@ -4,4 +4,21 @@
 
 extern bool enable_td_part;
 
+#ifdef CONFIG_INTEL_TD_PART_GUEST
+static inline bool is_td_part(struct kvm *kvm)
+{
+	return kvm->arch.vm_type == KVM_X86_TD_PART_VM;
+}
+
+static inline bool is_td_part_vcpu(struct kvm_vcpu *vcpu)
+{
+	return is_td_part(vcpu->kvm);
+}
+
+#else /* CONFIG_INTEL_TD_PART_GUEST */
+
+static inline bool is_td_part(struct kvm *kvm) { return false; }
+static inline bool is_td_part_vcpu(struct kvm_vcpu *vcpu) { return false; }
+#endif /* CONFIG_INTEL_TD_PART_GUEST */
+
 #endif /* __KVM_X86_TD_PART_H */
