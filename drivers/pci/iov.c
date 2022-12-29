intel/tdx

tdx: Disable SRIOV with device filter
SRIOV is a complex feature that would need significant work to harden.
Make sure SRIOV is completely disabled when the device filter
is active. Before this patch it was partially disabled but could
still be enabled by specific drivers. The virtio drivers can enable
SRIOV, so make sure it cannot be enabled.

Signed-off-by: Andi Kleen <ak@linux.intel.com>

drivers/pci/iov.c
@@ -12,6 +12,7 @@
 #include <linux/export.h>
 #include <linux/string.h>
 #include <linux/delay.h>
+#include <linux/cc_platform.h>
 #include "pci.h"
 
 #define VIRTFN_ID_LEN	16
@@ -568,6 +569,9 @@ static int sriov_enable(struct pci_dev *dev, int nr_virtfn)
 
 	if (!nr_virtfn)
 		return 0;
+
+	if (cc_platform_has(CC_ATTR_GUEST_DEVICE_FILTER))
+		return -EINVAL;
 
 	if (iov->num_VFs)
 		return -EINVAL;

intel/td-partitioning

drivers/pci/iov.c
@@ -12,6 +12,7 @@
 #include <linux/export.h>
 #include <linux/string.h>
 #include <linux/delay.h>
+#include <linux/cc_platform.h>
 #include "pci.h"
 
 #define VIRTFN_ID_LEN	16
@@ -609,6 +610,9 @@ static int sriov_enable(struct pci_dev *dev, int nr_virtfn)
 
 	if (!nr_virtfn)
 		return 0;
+
+	if (cc_platform_has(CC_ATTR_GUEST_HARDENED))
+		return -EINVAL;
 
 	if (iov->num_VFs)
 		return -EINVAL;