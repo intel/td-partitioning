9fffea3ed729889dc854b92b1c33952e8805496e
Wei Wang <wei.w.wang@intel.com>
2023-01-10 13:49:55 +0800

    KVM: TDX_MIG: migration stream create and release
    When userspace creates a KVM_DEV_TYPE_TDX_MIG_STREAM kvm_device,
    tdx_mig_stream_create is invoked to create a migration stream.
    Currently, only one migration stream is supported by KVM.
    
    The backward stream is special stream which is used when canceling
    migration during post-copy. It's not supported currently, but the TDX
    architecture requires it to be created though not used.
    
    When the kvm_device fd is closed, the related data structures and the
    stream's misc pages are reclaimed and freed.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/vmx/tdx_mig.c
+++ arch/x86/kvm/vmx/tdx_mig.c
@@ -2,6 +2,21 @@
 #include <linux/anon_inodes.h>
 #include <linux/kvm_host.h>
 
+struct tdx_mig_stream {
+	uint16_t idx;
+	unsigned long migsc_pa;
+};
+
+struct tdx_mig_state {
+	/* Migration (forward) stream to migrate the TD states */
+	struct tdx_mig_stream stream;
+	/*
+	 * Backward stream not used in the version. But required by the TDX
+	 * architecture to be created.
+	 */
+	struct tdx_mig_stream backward_stream;
+};
+
 struct tdx_mig_capabilities {
 	uint32_t max_migs;
 	uint32_t nonmem_state_pages;
@@ -69,9 +84,95 @@ static long tdx_mig_stream_ioctl(struct kvm_device *dev, unsigned int ioctl,
 	return -ENXIO;
 }
 
+static int tdx_mig_do_stream_create(struct kvm_tdx *kvm_tdx,
+				    struct tdx_mig_stream *stream)
+{
+	struct tdx_module_output out;
+	unsigned long migsc_va, migsc_pa;
+	uint64_t err;
+
+	migsc_va = __get_free_page(GFP_KERNEL_ACCOUNT);
+	if (!migsc_va)
+		return -ENOMEM;
+	migsc_pa = __pa(migsc_va);
+
+	err = tdh_mig_stream_create(kvm_tdx->tdr_pa, migsc_pa);
+	if (WARN_ON_ONCE(err)) {
+		pr_tdx_error(TDH_MIG_STREAM_CREATE, err, &out);
+		free_page(migsc_va);
+		return -EIO;
+	}
+	stream->migsc_pa = migsc_pa;
+
+	return 0;
+}
+
+static int tdx_mig_state_create(struct kvm_tdx *kvm_tdx)
+{
+	struct tdx_mig_state *mig_state;
+
+	/*
+	 * Current version supports only one migration stream. The mig_state
+	 * has been allocated when the stream is created.
+	 */
+	if (kvm_tdx->mig_state) {
+		pr_warn("only 1 migration stream supported currently\n");
+		return -EEXIST;
+	}
+
+	mig_state = kzalloc(sizeof(struct tdx_mig_state), GFP_KERNEL_ACCOUNT);
+	if (!mig_state)
+		return -ENOMEM;
+
+	if (tdx_mig_do_stream_create(kvm_tdx, &mig_state->backward_stream)) {
+		kfree(mig_state);
+		return -EIO;
+	}
+	kvm_tdx->mig_state = mig_state;
+	return 0;
+}
+
+static void tdx_mig_state_destroy(struct kvm_tdx *kvm_tdx)
+{
+	struct tdx_mig_state *mig_state =
+		(struct tdx_mig_state *)kvm_tdx->mig_state;
+
+	if (!mig_state)
+		return;
+
+	tdx_reclaim_td_page(mig_state->stream.migsc_pa);
+	tdx_reclaim_td_page(mig_state->backward_stream.migsc_pa);
+	kfree(mig_state);
+	kvm_tdx->mig_state = NULL;
+}
+
 static int tdx_mig_stream_create(struct kvm_device *dev, u32 type)
 {
-	return -ENXIO;
+	struct kvm_tdx *kvm_tdx = to_kvm_tdx(dev->kvm);
+	struct tdx_mig_state *mig_state;
+	struct tdx_mig_stream *stream;
+	int ret;
+
+	/*
+	 * At least two migration streams (forward stream + backward stream)
+	 * are required to be created.
+	 */
+	if (unlikely(tdx_mig_caps.max_migs < 2))
+		return -ENOENT;
+
+	ret = tdx_mig_state_create(kvm_tdx);
+	if (ret)
+		return ret;
+
+	mig_state = (struct tdx_mig_state *)kvm_tdx->mig_state;
+	stream = &mig_state->stream;
+	ret = tdx_mig_do_stream_create(kvm_tdx, stream);
+	if (ret)
+		return ret;
+
+	dev->private = stream;
+
+	return 0;
 }
 
 static void tdx_mig_stream_release(struct kvm_device *dev)
