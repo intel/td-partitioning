cc-api/linux

x86/virt/tdx: Initialize TDX module after TD-preserving updates
TDX module initialization after TD-preserving module updates shouldn't
configure global key, initialize TDMRs. Instead, the new module must
import the handoff data left by the previous one to preserve TDs.

Use TDH.SYS.UPDATE to import the handoff data.

Signed-off-by: Chao Gao <chao.gao@intel.com>

arch/x86/virt/vmx/tdx/tdx.c
@@ -1481,7 +1481,8 @@ static void shutdown_tdx_module(void)
 	tdx_module_sysfs_deinit();
 }
 
-static int __tdx_init_cpuslocked(void)
+static int init_tdx_module_via_handoff_data(void);
+static int __tdx_init_cpuslocked(bool preserving)
 {
 	int ret;
 
@@ -1498,7 +1499,10 @@ static int __tdx_init_cpuslocked(void)
 		return -EINVAL;
 	}
 
-	ret = init_tdx_module();
+	if (preserving)
+		ret = init_tdx_module_via_handoff_data();
+	else
+		ret = init_tdx_module();
 	if (ret == -ENODEV) {
 		pr_info("TDX module is not loaded.\n");
 		return ret;
@@ -1539,7 +1543,7 @@ static int __tdx_init(void)
 	 * any CPU from going offline during the initialization.
 	 */
 	cpus_read_lock();
-	ret = __tdx_init_cpuslocked();
+	ret = __tdx_init_cpuslocked(false);
 	cpus_read_unlock();
 
 	/*
@@ -1946,6 +1950,29 @@ static int tdx_prepare_handoff_data(u16 req_hv)
 	return ret;
 }
 
+static int init_tdx_module_via_handoff_data(void)
+{
+	int ret;
+
+	ret = init_tdx_module_common();
+	if (ret)
+		goto out;
+
+	ret = seamcall(TDH_SYS_UPDATE, 0, 0, 0, 0, NULL);
+	if (ret) {
+		pr_info("Failed to load handoff data");
+		goto out;
+	}
+
+	tdx_module_status = TDX_MODULE_INITIALIZED;
+	pr_info("TDX module initialized.\n");
+	return 0;
+
+out:
+	pr_info("Failed to initialize TDX module.\n");
+	return ret;
+}
+
 int tdx_module_update(const struct tmu_req *req)
 {
 	int ret;
@@ -1988,7 +2015,7 @@ int tdx_module_update(const struct tmu_req *req)
 	/* Initialize TDX module after a successful update */
 	if (!ret) {
 		tdx_module_status = TDX_MODULE_UNKNOWN;
-		ret = __tdx_init_cpuslocked();
+		ret = __tdx_init_cpuslocked(req->preserving);
 	}
 
 unlock:
@@ -1999,4 +2026,9 @@ free:
 	return ret;
 }
 EXPORT_SYMBOL_GPL(tdx_module_update);
+#else /* CONFIG_INTEL_TDX_MODULE_UPDATE */
+static int init_tdx_module_via_handoff_data(void)
+{
+	return -EOPNOTSUPP;
+}
 #endif /* CONFIG_INTEL_TDX_MODULE_UPDATE */

intel/td-partitioning

arch/x86/virt/vmx/tdx/tdx.c
@@ -1459,11 +1459,15 @@ out:
 	return ret;
 }
 
-static int __tdx_enable(void)
+static int init_tdx_module_via_handoff_data(void);
+static int __tdx_enable(bool preserving)
 {
 	int ret;
 
-	ret = init_tdx_module();
+	if (preserving)
+		ret = init_tdx_module_via_handoff_data();
+	else
+		ret = init_tdx_module();
 	if (ret) {
 		pr_err_once("initialization failed (%d)\n", ret);
 		tdx_module_status = TDX_MODULE_ERROR;
@@ -1505,7 +1509,7 @@ int tdx_enable(void)
 
 	switch (tdx_module_status) {
 	case TDX_MODULE_UNKNOWN:
-		ret = __tdx_enable();
+		ret = __tdx_enable(false);
 		break;
 	case TDX_MODULE_INITIALIZED:
 		/* Already initialized, great, tell the caller. */
@@ -1847,6 +1851,21 @@ static int tdx_prepare_handoff_data(u16 req_hv)
 	return seamcall(TDH_SYS_SHUTDOWN, req_hv, 0, 0, 0, NULL, NULL);
 }
 
+static int init_tdx_module_via_handoff_data(void)
+{
+	int ret;
+
+	ret = init_tdx_module_common();
+	if (ret)
+		return ret;
+
+	ret = seamcall(TDH_SYS_UPDATE, 0, 0, 0, 0, NULL, NULL);
+	if (ret)
+		pr_info("Failed to load handoff data");
+
+	return ret;
+}
+
 int tdx_module_update(bool live_update)
 {
 	int ret;
@@ -1910,7 +1929,7 @@ int tdx_module_update(bool live_update)
 	/* Initialize TDX module after a successful update */
 	if (!ret) {
 		tdx_module_status = TDX_MODULE_UNKNOWN;
-		ret = __tdx_enable();
+		ret = __tdx_enable(live_update);
 	}
 
 unlock:
@@ -1928,4 +1947,9 @@ unregister:
 	return ret;
 }
 EXPORT_SYMBOL_GPL(tdx_module_update);
+#else /* CONFIG_INTEL_TDX_MODULE_UPDATE */
+static int init_tdx_module_via_handoff_data(void)
+{
+	return -EOPNOTSUPP;
+}
 #endif /* CONFIG_INTEL_TDX_MODULE_UPDATE */