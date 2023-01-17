530a2111268c9a92fd6049bbfb8c9117a5bb9fc3
Wei Wang <wei.w.wang@intel.com>
2023-01-17 10:30:03 +0800

    x86/tdx: retry tdcalls on TDX_OPERAND_BUSY related errors
    TDX_OPERAND_BUSY and TDX_OPERAND_BUSY_HOST_PRIORITY errors are returned
    to the guest when its tdcall operations fail to acquire some TDX internal
    resources (e.g. locks), and the guest is expected to retry the operations.
    
    Limit the number of retires to TDX_HYPERCALL_RETRY_MAX, and exceeding
    this number is deemed as an issue and need to fail out.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/include/asm/shared/tdx.h
+++ arch/x86/include/asm/shared/tdx.h
@@ -31,6 +31,12 @@
 #define TDVMCALL_REPORT_FATAL_ERROR	0x10003
 #define TDVMCALL_SETUP_NOTIFY_INTR	0x10004
 
+#define TDX_HYPERCALL_RETRY_MAX		10000
+#define TDX_HYPERCALL_STATUS_MASK	0xFFFFFFFF00000000ULL
+
+#define TDX_OPERAND_BUSY		0x8000020000000000ULL
+#define TDX_OPERAND_BUSY_HOST_PRIORITY	0x8000020400000000ULL
+
 #ifndef __ASSEMBLY__
 
 /*
@@ -55,7 +61,23 @@ struct tdx_hypercall_args {
 };
 
 /* Used to request services from the VMM */
-u64 __tdx_hypercall(struct tdx_hypercall_args *args, unsigned long flags);
+u64 __do_tdx_hypercall(struct tdx_hypercall_args *args, unsigned long flags);
+
+static inline u64 __tdx_hypercall(struct tdx_hypercall_args *args, unsigned long flags)
+{
+	u64 err, retries = 0;
+
+	do {
+		err = __do_tdx_hypercall(args, flags);
+		if (likely(!err) || retries++ > TDX_HYPERCALL_RETRY_MAX)
+			break;
+
+		err &= TDX_HYPERCALL_STATUS_MASK;
+	} while (err == TDX_OPERAND_BUSY ||
+		 err == TDX_OPERAND_BUSY_HOST_PRIORITY);
+
+	return err;
+}
 
 /*
  * Wrapper for standard use of __tdx_hypercall with no output aside from
