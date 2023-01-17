52ea4367abfba483c9fb67afa7ea8dce6fbd6d02
Wei Wang <wei.w.wang@intel.com>
2023-01-17 10:49:03 +0800

    KVM: TDX: allow userspace to finish td initialization at a later stage
    Add KVM_TDX_INIT_VM_F_POST_INIT to allow userspace to finish TD
    initialization at a later stage in tdx_td_post_init. The usage examaple
    is that during live migration, the destination TD states are not fully
    initialized with the parameters from the kvm_tdx_init_vm struct.
    Instead, most of the TD states are initialized with the values migrated
    from the source side. So allow userspace to set the flag to delay the
    finishing of TD initialization in this case.
    
    The td_initialized field is added to inidicate the TD is done
    initialization.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -3562,7 +3562,6 @@ static int setup_tdparams(struct kvm *kvm, struct td_params *td_params,
 static int __tdx_td_init(struct kvm *kvm, struct td_params *td_params)
 {
 	struct kvm_tdx *kvm_tdx = to_kvm_tdx(kvm);
-	struct tdx_module_output out;
 	cpumask_var_t packages;
 	unsigned long *tdcs_pa = NULL;
 	unsigned long tdr_pa = 0;
@@ -3682,13 +3681,6 @@ static int __tdx_td_init(struct kvm *kvm, struct td_params *td_params)
 		}
 	}
 
-	err = tdh_mng_init(kvm_tdx->tdr_pa, __pa(td_params), &out);
-	if (WARN_ON_ONCE(err)) {
-		pr_tdx_error(TDH_MNG_INIT, err, &out);
-		ret = -EIO;
-		goto teardown;
-	}
-
 	return 0;
 
 	/*
@@ -3723,11 +3715,21 @@ free_hkid:
 	return ret;
 }
 
+static int tdx_td_post_init(struct kvm_tdx *kvm_tdx)
+{
+	kvm_tdx->tsc_offset = td_tdcs_exec_read64(kvm_tdx, TD_TDCS_EXEC_TSC_OFFSET);
+	kvm_tdx->td_initialized = true;
+
+	return 0;
+}
+
 static int tdx_td_init(struct kvm *kvm, struct kvm_tdx_cmd *cmd)
 {
 	struct kvm_tdx *kvm_tdx = to_kvm_tdx(kvm);
 	struct kvm_tdx_init_vm *init_vm = NULL;
 	struct td_params *td_params = NULL;
+	struct tdx_module_output out;
+	u64 err;
 	void *entries_end;
 	int ret;
 
@@ -3739,7 +3741,7 @@ static int tdx_td_init(struct kvm *kvm, struct kvm_tdx_cmd *cmd)
 	if (is_hkid_assigned(kvm_tdx))
 		return -EINVAL;
 
-	if (cmd->flags)
+	if (cmd->flags && cmd->flags != KVM_TDX_INIT_VM_F_POST_INIT)
 		return -EINVAL;
 
 	init_vm = kzalloc(sizeof(*init_vm), GFP_KERNEL);
@@ -3775,7 +3777,6 @@ static int tdx_td_init(struct kvm *kvm, struct kvm_tdx_cmd *cmd)
 	if (ret)
 		goto out;
 
-	kvm_tdx->tsc_offset = td_tdcs_exec_read64(kvm_tdx, TD_TDCS_EXEC_TSC_OFFSET);
 	kvm_tdx->attributes = td_params->attributes;
 	kvm_tdx->xfam = td_params->xfam;
 
@@ -3783,6 +3784,16 @@ static int tdx_td_init(struct kvm *kvm, struct kvm_tdx_cmd *cmd)
 		kvm->arch.gfn_shared_mask = gpa_to_gfn(BIT_ULL(51));
 	else
 		kvm->arch.gfn_shared_mask = gpa_to_gfn(BIT_ULL(47));
+
+	if (cmd->flags != KVM_TDX_INIT_VM_F_POST_INIT) {
+		err = tdh_mng_init(kvm_tdx->tdr_pa, __pa(td_params), &out);
+		if (WARN_ON_ONCE(err)) {
+			pr_tdx_error(TDH_MNG_INIT, err, &out);
+			ret = -EIO;
+			goto out;
+		}
+		tdx_td_post_init(kvm_tdx);
+	}
 
 out:
 	/* kfree() accepts NULL. */
