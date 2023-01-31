1c20b98994be48eea2b9be546db9d75cef8d7d45
Wei Wang <wei.w.wang@intel.com>
2023-01-31 22:02:56 +0800

    KVM: TDX: enable write-unblocking through fast page fault
    Change the EPT violation handler to check the exit_qualification bits
    returned from the TDX module, and this will allow kvm mmu to detect if
    this is a fault due to write-blocking and handle it through fast page
    fault.
    
    Add the tdx write-unblocking API to x86_ops so that it will be invoked
    from kvm mmu.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/vmx/tdx_mig.c
+++ arch/x86/kvm/vmx/tdx_mig.c
@@ -115,6 +115,17 @@ struct tdx_mig_mac_list {
 	hpa_t hpa;
 };
 
+/* Secure EPT mapping info used by TDH_EXPORT_UNBLOCKW */
+union tdx_mig_ept_info {
+	uint64_t val;
+	struct {
+		uint64_t level	: 3;
+		uint64_t rsvd1	: 9;
+		uint64_t gfn	: 40;
+		uint64_t rsvd2	: 12;
+	};
+};
+
 struct tdx_mig_stream {
 	uint16_t idx;
 	unsigned long migsc_pa;
@@ -247,6 +258,27 @@ static void tdx_write_block_private_pages(struct kvm *kvm, gfn_t *gfns,
 	}
 
 	WRITE_ONCE(kvm_tdx->has_range_blocked, true);
+}
+
+static void tdx_write_unblock_private_page(struct kvm *kvm,
+					  gfn_t gfn, int level)
+{
+	struct tdx_module_output out;
+	struct kvm_tdx *kvm_tdx = to_kvm_tdx(kvm);
+	union tdx_mig_ept_info ept_info = {
+		/*
+		 * TDX treats level 0 as the leaf level, while Linux treats
+		 * level 1 (PG_LEVEL_4K) as the level.
+		 */
+		.level = pg_level_to_tdx_sept_level(level),
+		.rsvd1 = 0,
+		.gfn = gfn,
+		.rsvd2 = 0,
+	};
+
+	tdx_track(kvm_tdx);
+
+	tdh_export_unblockw(kvm_tdx->tdr_pa, ept_info.val, &out);
 }
 
 static void tdx_mig_stream_get_tdx_mig_attr(struct tdx_mig_stream *stream,
