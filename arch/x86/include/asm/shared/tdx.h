7f34c39398f7e8ef07aead89ec0169f2967e12e3
Wei Wang <wei.w.wang@intel.com>
2023-03-08 14:17:38 +0800

    x86/tdx: fix retry tdcalls on TDX_OPERAND_BUSY
    Retry should be added to tdx_module_call instead of tdx_hypercall.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/include/asm/shared/tdx.h
+++ arch/x86/include/asm/shared/tdx.h
@@ -31,8 +31,8 @@
 #define TDVMCALL_REPORT_FATAL_ERROR	0x10003
 #define TDVMCALL_SETUP_NOTIFY_INTR	0x10004
 
-#define TDX_HYPERCALL_RETRY_MAX		10000
-#define TDX_HYPERCALL_STATUS_MASK	0xFFFFFFFF00000000ULL
+#define TDX_MODULECALL_RETRY_MAX	10000
+#define TDX_MODULECALL_STATUS_MASK	0xFFFFFFFF00000000ULL
 
 #define TDX_OPERAND_BUSY		0x8000020000000000ULL
 #define TDX_OPERAND_BUSY_HOST_PRIORITY	0x8000020400000000ULL
@@ -61,23 +61,7 @@ struct tdx_hypercall_args {
 };
 
 /* Used to request services from the VMM */
-u64 __do_tdx_hypercall(struct tdx_hypercall_args *args, unsigned long flags);
-
-static inline u64 __tdx_hypercall(struct tdx_hypercall_args *args, unsigned long flags)
-{
-	u64 err, retries = 0;
-
-	do {
-		err = __do_tdx_hypercall(args, flags);
-		if (likely(!err) || retries++ > TDX_HYPERCALL_RETRY_MAX)
-			break;
-
-		err &= TDX_HYPERCALL_STATUS_MASK;
-	} while (err == TDX_OPERAND_BUSY ||
-		 err == TDX_OPERAND_BUSY_HOST_PRIORITY);
-
-	return err;
-}
+u64 __tdx_hypercall(struct tdx_hypercall_args *args, unsigned long flags);
 
 /*
  * Wrapper for standard use of __tdx_hypercall with no output aside from
@@ -96,7 +80,6 @@ static inline u64 _tdx_hypercall(u64 fn, u64 r12, u64 r13, u64 r14, u64 r15)
 
 	return __tdx_hypercall(&args, 0);
 }
-
 
 /* Called from __tdx_hypercall() for unrecoverable failure */
 void __tdx_hypercall_failed(void);
@@ -118,9 +101,28 @@ struct tdx_module_output {
 };
 
 /* Used to communicate with the TDX module */
-u64 __tdx_module_call(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
-		      u64 r10, u64 r11, u64 r12, u64 r13,
-		      struct tdx_module_output *out);
+u64 __tdx_module_call_asm(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
+			  u64 r10, u64 r11, u64 r12, u64 r13,
+			  struct tdx_module_output *out);
+
+static inline u64 __tdx_module_call(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
+				    u64 r10, u64 r11, u64 r12, u64 r13,
+				    struct tdx_module_output *out)
+{
+	u64 err, err_masked, retries = 0;
+
+	do {
+		err = __tdx_module_call_asm(fn, rcx, rdx, r8, r9,
+					    r10, r11, r12, r13, out);
+		if (likely(!err) || retries++ > TDX_MODULECALL_RETRY_MAX)
+			break;
+
+		err_masked = err & TDX_MODULECALL_STATUS_MASK;
+	} while (err_masked == TDX_OPERAND_BUSY ||
+		 err_masked == TDX_OPERAND_BUSY_HOST_PRIORITY);
+
+	return err;
+}
 
 void tdx_accept_memory(phys_addr_t start, phys_addr_t end);
 
