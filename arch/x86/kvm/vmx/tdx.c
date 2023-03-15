4dff03c2e760076f760fce09b19b103b2bc06131
Chuanxiao Dong <chuanxiao.dong@intel.com>
2023-03-15 10:48:25 +0800

    REVERTME: KVM: TDX: Use the same shared EPT for L1 and L2
    Use the same shared EPT for L1 and L2 are much easier than using
    different ones. Currently use this solution but later will implement
    dedicated shared EPT for each L2.
    
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -2351,6 +2351,7 @@ static int handle_tdvmcall(struct kvm_vcpu *vcpu)
 void tdx_load_mmu_pgd(struct kvm_vcpu *vcpu, hpa_t root_hpa, int pgd_level)
 {
 	struct kvm_tdx *kvm_tdx = to_kvm_tdx(vcpu->kvm);
+	int i;
 
 	if (!kvm_tdx->td_initialized) {
 		vcpu->load_mmu_pgd_pending = true;
@@ -2359,6 +2360,9 @@ void tdx_load_mmu_pgd(struct kvm_vcpu *vcpu, hpa_t root_hpa, int pgd_level)
 
 	vcpu->load_mmu_pgd_pending = false;
 	td_vmcs_write64(to_tdx(vcpu), SHARED_EPT_POINTER, root_hpa & PAGE_MASK);
+	for (i = 0; i < kvm_tdx->num_l2_vms; i++)
+		l2td_vmcs_write64(to_tdx(vcpu), SHARED_EPT_POINTER,
+				  root_hpa & PAGE_MASK, i);
 }
 
 static void tdx_measure_page(struct kvm_tdx *kvm_tdx, hpa_t gpa, int size)
