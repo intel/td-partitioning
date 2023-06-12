8347dad47a9ffbac8648329e06bbf4f748050351
Xiaocheng Dong <xiaocheng.dong@intel.com>
2023-06-12 03:40:45 -0400

    KVM: x86/tdp_mmu: Fix merging private SPT return and its handler mismatch
    The handler tdp_mmu_map_handle_target_level check return value of
    tdp_mmu_merge_private_spt as errno, but the return value was using enum
    RET_PF_RETRY, change back to -EBUSY for consistency.
    
    Signed-off-by: Xiaocheng Dong <xiaocheng.dong@intel.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -1565,7 +1565,7 @@ static int tdp_mmu_merge_private_spt(struct kvm_vcpu *vcpu,
 			goto out;
 	}
 	if (ret_pf_retry) {
-		ret = RET_PF_RETRY;
+		ret = -EBUSY;
 		goto out;
 	}
 
@@ -1583,7 +1583,7 @@ static int tdp_mmu_merge_private_spt(struct kvm_vcpu *vcpu,
 	 * pending.  Since the child page was mapped above, let vcpu run.
 	 */
 	if (ret == -EAGAIN)
-		ret = RET_PF_RETRY;
+		ret = -EBUSY;
 	if (ret)
 		goto unzap;
 
@@ -1596,7 +1596,7 @@ static int tdp_mmu_merge_private_spt(struct kvm_vcpu *vcpu,
 	 */
 	tdp_unaccount_mmu_page(kvm, child_sp);
 	tdp_mmu_free_sp(child_sp);
-	return RET_PF_RETRY;
+	return -EBUSY;
 
 unzap:
 	if (static_call(kvm_x86_unzap_private_spte)(kvm, gfn, level))
@@ -1662,7 +1662,7 @@ static int tdp_mmu_map_handle_target_level(struct kvm_vcpu *vcpu,
 		ret = RET_PF_SPURIOUS;
 	else {
 		ret = __tdp_mmu_map_handle_target_level(vcpu, fault, iter, new_spte);
-		if (ret == -EBUSY)
+		if (ret == -EBUSY || ret == -EAGAIN)
 			return RET_PF_RETRY;
 		else if (ret)
 			return RET_PF_INVALID;
