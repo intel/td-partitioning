intel/tdx

KVM: Introduce the KVM_MEMORY_ENCRYPT_{READ,WRITE}_MEMORY VM ioctl
These 2 new VM level ioctls focus on memory read/write from/to
encrypted guest like INTEL TDX guest or AMD SEV guest for
debugging purpose. Userspace VMM uses these interface to get
content of guest memory, e.g. supporing "x"/"xp" commands
in QEMU monitor or supporting QEMU's guest debugging feature.

Also introduce 2 new interfaces in kvm_x86_ops to handle the
new ioctls.

int (*mem_enc_read_memory)(struct kvm *kvm,
                           struct kvm_rw_memory *argp);
int (*mem_enc_write_memory)(struct kvm *kvm,
                            struct kvm_rw_memory *argp);

Signed-off-by: Yuan Yao <yuan.yao@intel.com>

arch/x86/kvm/x86.c
@@ -4410,6 +4410,11 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 		if (static_call(kvm_x86_is_vm_type_supported)(KVM_X86_TDX_VM))
 			r |= BIT(KVM_X86_TDX_VM);
 		break;
+	case KVM_CAP_ENCRYPT_MEMORY_DEBUG:
+		if (kvm_x86_ops.mem_enc_read_memory &&
+		    kvm_x86_ops.mem_enc_write_memory)
+			r = 1;
+		break;
 	default:
 		break;
 	}
@@ -6649,6 +6654,40 @@ set_pit2_out:
 			goto out;
 
 		r = static_call(kvm_x86_mem_enc_unregister_region)(kvm, &region);
+		break;
+	}
+	case KVM_MEMORY_ENCRYPT_READ_MEMORY: {
+		struct kvm_rw_memory rw;
+		struct kvm_rw_memory *rw_user = argp;
+
+		r = -EFAULT;
+		if (copy_from_user(&rw, argp, sizeof(rw)))
+			goto out;
+
+		r = -ENOTTY;
+		if (kvm_x86_ops.mem_enc_read_memory) {
+			r = static_call(kvm_x86_mem_enc_read_memory)(kvm, &rw);
+			if (copy_to_user(&rw_user->len, &rw.len,
+					 sizeof(rw.len)))
+				r = -EFAULT;
+		}
+		break;
+	}
+	case KVM_MEMORY_ENCRYPT_WRITE_MEMORY: {
+		struct kvm_rw_memory rw;
+		struct kvm_rw_memory *rw_user = argp;
+
+		r = -EFAULT;
+		if (copy_from_user(&rw, argp, sizeof(rw)))
+			goto out;
+
+		r = -ENOTTY;
+		if (kvm_x86_ops.mem_enc_write_memory) {
+			r = static_call(kvm_x86_mem_enc_write_memory)(kvm, &rw);
+			if (copy_to_user(&rw_user->len, &rw.len,
+					 sizeof(rw.len)))
+				r = -EFAULT;
+		}
 		break;
 	}
 	case KVM_HYPERV_EVENTFD: {

intel/td-partitioning

arch/x86/kvm/x86.c
@@ -4577,6 +4577,11 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 		if (static_call(kvm_x86_is_vm_type_supported)(KVM_X86_TDX_VM))
 			r |= BIT(KVM_X86_TDX_VM);
 		break;
+	case KVM_CAP_ENCRYPT_MEMORY_DEBUG:
+		if (kvm_x86_ops.mem_enc_read_memory &&
+		    kvm_x86_ops.mem_enc_write_memory)
+			r = 1;
+		break;
 	default:
 		break;
 	}
@@ -7039,6 +7044,40 @@ set_pit2_out:
 			goto out;
 
 		r = static_call(kvm_x86_mem_enc_unregister_region)(kvm, &region);
+		break;
+	}
+	case KVM_MEMORY_ENCRYPT_READ_MEMORY: {
+		struct kvm_rw_memory rw;
+		struct kvm_rw_memory __user *rw_user = (void __user *)argp;
+
+		r = -EFAULT;
+		if (copy_from_user(&rw, rw_user, sizeof(rw)))
+			goto out;
+
+		r = -ENOTTY;
+		if (kvm_x86_ops.mem_enc_read_memory) {
+			r = static_call(kvm_x86_mem_enc_read_memory)(kvm, &rw);
+			if (copy_to_user(&rw_user->len, &rw.len,
+					 sizeof(rw.len)))
+				r = -EFAULT;
+		}
+		break;
+	}
+	case KVM_MEMORY_ENCRYPT_WRITE_MEMORY: {
+		struct kvm_rw_memory rw;
+		struct kvm_rw_memory __user *rw_user = (void __user *)argp;
+
+		r = -EFAULT;
+		if (copy_from_user(&rw, rw_user, sizeof(rw)))
+			goto out;
+
+		r = -ENOTTY;
+		if (kvm_x86_ops.mem_enc_write_memory) {
+			r = static_call(kvm_x86_mem_enc_write_memory)(kvm, &rw);
+			if (copy_to_user(&rw_user->len, &rw.len,
+					 sizeof(rw.len)))
+				r = -EFAULT;
+		}
 		break;
 	}
 	case KVM_HYPERV_EVENTFD: {