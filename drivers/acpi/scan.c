intel/tdx

ACPI: Initialize authorized attribute for confidential guest
Confidential guest platforms like TDX have a requirement to allow
only trusted devices. So initialize the "authorized" attribute
using cc_guest_dev_authorized().

cc_guest_dev_authorized() will consult ARCH specific allow list
to return authorization status of the given device.

Add support to initialize the ACPI bus authorized attribute using
cc_guest_dev_authorized() API.

Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

drivers/acpi/scan.c
@@ -20,6 +20,7 @@
 #include <linux/platform_data/x86/apple.h>
 #include <linux/pgtable.h>
 #include <linux/crc32.h>
+#include <linux/cc_platform.h>
 
 #include "internal.h"
 
@@ -748,6 +749,10 @@ static int __acpi_device_add(struct acpi_device *device,
 
 	device->dev.bus = &acpi_bus_type;
 	device->dev.release = release;
+
+	if (cc_platform_has(CC_ATTR_GUEST_DEVICE_FILTER))
+		device->dev.authorized = cc_guest_dev_authorized(&device->dev);
+
 	result = device_add(&device->dev);
 	if (result) {
 		dev_err(&device->dev, "Error registering device\n");

intel/td-partitioning

drivers/acpi/scan.c
@@ -21,6 +21,7 @@
 #include <linux/pgtable.h>
 #include <linux/crc32.h>
 #include <linux/dma-direct.h>
+#include <linux/cc_platform.h>
 
 #include "internal.h"
 
@@ -727,6 +728,9 @@ int acpi_device_add(struct acpi_device *device)
 	acpi_store_pld_crc(device);
 
 	mutex_unlock(&acpi_device_lock);
+
+	if (cc_platform_has(CC_ATTR_GUEST_DEVICE_FILTER))
+		device->dev.authorized = arch_dev_authorized(&device->dev);
 
 	result = device_add(&device->dev);
 	if (result) {