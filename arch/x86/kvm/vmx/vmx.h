intel/tdx

KVM: TDX: add KVM_BUG_ON to to_kvm_vmx, to_vmx, to_kvm_tdx, to_tdx
Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kvm/vmx/vmx.h
@@ -651,11 +651,13 @@ static inline unsigned long vmx_l1_guest_owned_cr0_bits(void)
 
 static __always_inline struct kvm_vmx *to_kvm_vmx(struct kvm *kvm)
 {
+	KVM_BUG_ON(kvm->arch.vm_type == KVM_X86_PROTECTED_VM, kvm);
 	return container_of(kvm, struct kvm_vmx, kvm);
 }
 
 static __always_inline struct vcpu_vmx *to_vmx(struct kvm_vcpu *vcpu)
 {
+	KVM_BUG_ON(vcpu->kvm->arch.vm_type == KVM_X86_PROTECTED_VM, vcpu->kvm);
 	return container_of(vcpu, struct vcpu_vmx, vcpu);
 }
 

intel/td-partitioning

arch/x86/kvm/vmx/vmx.h
@@ -651,11 +651,13 @@ static inline unsigned long vmx_l1_guest_owned_cr0_bits(void)
 
 static inline struct kvm_vmx *to_kvm_vmx(struct kvm *kvm)
 {
+	KVM_BUG_ON(kvm->arch.vm_type == KVM_X86_TDX_VM, kvm);
 	return container_of(kvm, struct kvm_vmx, kvm);
 }
 
 static inline struct vcpu_vmx *to_vmx(struct kvm_vcpu *vcpu)
 {
+	KVM_BUG_ON(vcpu->kvm->arch.vm_type == KVM_X86_TDX_VM, vcpu->kvm);
 	return container_of(vcpu, struct vcpu_vmx, vcpu);
 }
 