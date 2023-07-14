intel/tdx

KVM: TDX: Support set DR7 for DEBUG TDX guest
Hardware breakpoints are working now for KVM's
guest debug feature.

Signed-off-by: Yuan Yao <yuan.yao@intel.com>
Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kvm/vmx/tdx.c
@@ -1090,6 +1090,16 @@ static int tdx_handle_exception(struct kvm_vcpu *vcpu)
 	return -EFAULT;
 }
 
+void tdx_set_dr7(struct kvm_vcpu *vcpu, unsigned long val)
+{
+	struct vcpu_tdx *tdx = to_tdx(vcpu);
+
+	if (!is_debug_td(vcpu) || !tdx->initialized)
+		return;
+
+	td_vmcs_write64(tdx, GUEST_DR7, val);
+}
+
 static int tdx_handle_external_interrupt(struct kvm_vcpu *vcpu)
 {
 	++vcpu->stat.irq_exits;

intel/td-partitioning

arch/x86/kvm/vmx/tdx.c
@@ -996,6 +996,16 @@ static int tdx_handle_exception(struct kvm_vcpu *vcpu)
 	return -EFAULT;
 }
 
+void tdx_set_dr7(struct kvm_vcpu *vcpu, unsigned long val)
+{
+	struct vcpu_tdx *tdx = to_tdx(vcpu);
+
+	if (!is_debug_td(vcpu) || !tdx->vcpu_initialized)
+		return;
+
+	td_vmcs_write64(tdx, GUEST_DR7, val);
+}
+
 static int tdx_handle_external_interrupt(struct kvm_vcpu *vcpu)
 {
 	++vcpu->stat.irq_exits;