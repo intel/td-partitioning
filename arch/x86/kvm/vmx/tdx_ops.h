intel/tdx

KVM: TDX: protect TDX pages(TDR, TDCX, TDVPR, TDVPX)
Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kvm/vmx/tdx_ops.h
@@ -8,6 +8,8 @@
 
 #include <asm/pgtable_types.h>
 #include <asm/cacheflush.h>
+#include <asm/set_memory.h>
+#include <asm/tlbflush.h>
 #include <asm/asm.h>
 #include <asm/kvm_host.h>
 #include <asm/tdx.h>
@@ -49,6 +51,36 @@ static inline void tdx_clflush_page(hpa_t addr, enum pg_level level)
 	clflush_cache_range(__va(addr), KVM_HPAGE_SIZE(level));
 }
 
+static inline void tdx_set_page_np(hpa_t addr)
+{
+	if (!IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		return;
+
+	/* set_page_np() doesn't work due to non-preemptive context. */
+	set_direct_map_invalid_noflush(pfn_to_page(addr >> PAGE_SHIFT));
+	preempt_disable();
+	__flush_tlb_all();
+	preempt_enable();
+	arch_flush_lazy_mmu_mode();
+}
+
+static inline void tdx_set_page_present(hpa_t addr)
+{
+	if (IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		set_direct_map_default_noflush(pfn_to_page(addr >> PAGE_SHIFT));
+}
+
+static inline void tdx_set_page_present_level(hpa_t addr, enum pg_level pg_level)
+{
+	int i;
+
+	if (!IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		return;
+
+	for (i = 0; i < KVM_PAGES_PER_HPAGE(pg_level); i++)
+		set_direct_map_default_noflush(pfn_to_page((addr >> PAGE_SHIFT) + i));
+}
+
 /*
  * TDX module acquires its internal lock for resources.  It doesn't spin to get
  * locks because of its restrictions of allowed execution time.  Instead, it
@@ -81,8 +113,13 @@ static inline u64 tdx_seamcall_sept(u64 op, u64 rcx, u64 rdx, u64 r8, u64 r9,
 
 static inline u64 tdh_mng_addcx(hpa_t tdr, hpa_t addr)
 {
+	u64 r;
+
 	tdx_clflush_page(addr, PG_LEVEL_4K);
-	return tdx_seamcall(TDH_MNG_ADDCX, addr, tdr, 0, 0, NULL);
+	r = tdx_seamcall(TDH_MNG_ADDCX, addr, tdr, 0, 0, NULL);
+	if (!r)
+		tdx_set_page_np(addr);
+	return r;
 }
 
 static inline u64 tdh_mem_page_add(hpa_t tdr, gpa_t gpa, int level, hpa_t hpa,
@@ -107,8 +144,13 @@ static inline u64 tdh_mem_sept_remove(hpa_t tdr, gpa_t gpa, int level,
 
 static inline u64 tdh_vp_addcx(hpa_t tdvpr, hpa_t addr)
 {
+	u64 r;
+
 	tdx_clflush_page(addr, PG_LEVEL_4K);
-	return tdx_seamcall(TDH_VP_ADDCX, addr, tdvpr, 0, 0, NULL);
+	r = tdx_seamcall(TDH_VP_ADDCX, addr, tdvpr, 0, 0, NULL);
+	if (!r)
+		tdx_set_page_np(addr);
+	return r;
 }
 
 static inline u64 tdh_mem_page_relocate(hpa_t tdr, gpa_t gpa, hpa_t hpa,
@@ -138,14 +180,24 @@ static inline u64 tdh_mng_key_config(hpa_t tdr)
 
 static inline u64 tdh_mng_create(hpa_t tdr, int hkid)
 {
+	u64 r;
+
 	tdx_clflush_page(tdr, PG_LEVEL_4K);
-	return tdx_seamcall(TDH_MNG_CREATE, tdr, hkid, 0, 0, NULL);
+	r = tdx_seamcall(TDH_MNG_CREATE, tdr, hkid, 0, 0, NULL);
+	if (!r)
+		tdx_set_page_np(tdr);
+	return r;
 }
 
 static inline u64 tdh_vp_create(hpa_t tdr, hpa_t tdvpr)
 {
+	u64 r;
+
 	tdx_clflush_page(tdvpr, PG_LEVEL_4K);
-	return tdx_seamcall(TDH_VP_CREATE, tdvpr, tdr, 0, 0, NULL);
+	r = tdx_seamcall(TDH_VP_CREATE, tdvpr, tdr, 0, 0, NULL);
+	if (!r)
+		tdx_set_page_np(tdvpr);
+	return r;
 }
 
 static inline u64 tdh_mem_rd(hpa_t tdr, gpa_t addr, struct tdx_module_output *out)

intel/td-partitioning

arch/x86/kvm/vmx/tdx_ops.h
@@ -8,6 +8,8 @@
 
 #include <asm/pgtable_types.h>
 #include <asm/cacheflush.h>
+#include <asm/set_memory.h>
+#include <asm/tlbflush.h>
 #include <asm/asm.h>
 #include <asm/kvm_host.h>
 #include <asm/tdx.h>
@@ -27,6 +29,36 @@ static inline enum pg_level tdx_sept_level_to_pg_level(int tdx_level)
 static inline void tdx_clflush_page(hpa_t addr, enum pg_level level)
 {
 	clflush_cache_range(__va(addr), KVM_HPAGE_SIZE(level));
+}
+
+static inline void tdx_set_page_np(hpa_t addr)
+{
+	if (!IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		return;
+
+	/* set_page_np() doesn't work due to non-preemptive context. */
+	set_direct_map_invalid_noflush(pfn_to_page(addr >> PAGE_SHIFT));
+	preempt_disable();
+	__flush_tlb_all();
+	preempt_enable();
+	arch_flush_lazy_mmu_mode();
+}
+
+static inline void tdx_set_page_present(hpa_t addr)
+{
+	if (IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		set_direct_map_default_noflush(pfn_to_page(addr >> PAGE_SHIFT));
+}
+
+static inline void tdx_set_page_present_level(hpa_t addr, enum pg_level pg_level)
+{
+	int i;
+
+	if (!IS_ENABLED(CONFIG_INTEL_TDX_HOST_DEBUG_MEMORY_CORRUPT))
+		return;
+
+	for (i = 0; i < KVM_PAGES_PER_HPAGE(pg_level); i++)
+		set_direct_map_default_noflush(pfn_to_page((addr >> PAGE_SHIFT) + i));
 }
 
 /*
@@ -61,8 +93,13 @@ static inline u64 seamcall_sept(u64 op, u64 rcx, u64 rdx, u64 r8, u64 r9,
 
 static inline u64 tdh_mng_addcx(hpa_t tdr, hpa_t addr)
 {
+	u64 r;
+
 	tdx_clflush_page(addr, PG_LEVEL_4K);
-	return __seamcall(TDH_MNG_ADDCX, addr, tdr, 0, 0, NULL);
+	r = __seamcall(TDH_MNG_ADDCX, addr, tdr, 0, 0, NULL);
+	if (!r)
+		tdx_set_page_np(addr);
+	return r;
 }
 
 static inline u64 tdh_mem_page_add(hpa_t tdr, gpa_t gpa, int level, hpa_t hpa,
@@ -87,8 +124,13 @@ static inline u64 tdh_mem_sept_remove(hpa_t tdr, gpa_t gpa, int level,
 
 static inline u64 tdh_vp_addcx(hpa_t tdvpr, hpa_t addr)
 {
+	u64 r;
+
 	tdx_clflush_page(addr, PG_LEVEL_4K);
-	return __seamcall(TDH_VP_ADDCX, addr, tdvpr, 0, 0, NULL);
+	r = __seamcall(TDH_VP_ADDCX, addr, tdvpr, 0, 0, NULL);
+	if (!r)
+		tdx_set_page_np(addr);
+	return r;
 }
 
 static inline u64 tdh_mem_page_relocate(hpa_t tdr, gpa_t gpa, hpa_t hpa,
@@ -118,14 +160,24 @@ static inline u64 tdh_mng_key_config(hpa_t tdr)
 
 static inline u64 tdh_mng_create(hpa_t tdr, int hkid)
 {
+	u64 r;
+
 	tdx_clflush_page(tdr, PG_LEVEL_4K);
-	return __seamcall(TDH_MNG_CREATE, tdr, hkid, 0, 0, NULL);
+	r = __seamcall(TDH_MNG_CREATE, tdr, hkid, 0, 0, NULL);
+	if (!r)
+		tdx_set_page_np(tdr);
+	return r;
 }
 
 static inline u64 tdh_vp_create(hpa_t tdr, hpa_t tdvpr)
 {
+	u64 r;
+
 	tdx_clflush_page(tdvpr, PG_LEVEL_4K);
-	return __seamcall(TDH_VP_CREATE, tdvpr, tdr, 0, 0, NULL);
+	r = __seamcall(TDH_VP_CREATE, tdvpr, tdr, 0, 0, NULL);
+	if (!r)
+		tdx_set_page_np(tdvpr);
+	return r;
 }
 
 static inline u64 tdh_mem_rd(hpa_t tdr, gpa_t addr, struct tdx_module_output *out)