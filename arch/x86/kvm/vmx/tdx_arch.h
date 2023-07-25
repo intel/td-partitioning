intel/td-partitioning

commit 71bb594717d176a8c1e99b7a15533bdfbf0ae7dd

KVM: TDX: config VM_CTLS for L2 VM
The bit0 of VM_CTLS is applicable for L2 VMs only. If set, a TDCALL flow
that encounters an SEPT walk error causes a TD exit. Else, it returns an
error code to the guest. As host VMM is expected to handle the L2 SEPT
walking failure when L1 VMM using TDCALL TDG.MEM.PAGE.ATTR.WR to setup
the leaf entry, the bit0 should be set.

Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/tdx_arch.h
@@ -22,6 +22,7 @@
 #define TDH_VP_CREATE			10
 #define TDH_MNG_RD			11
 #define TDH_MEM_RD			12
+#define TDH_MNG_WR			13
 #define TDH_MEM_WR			14
 #define TDH_MEM_PAGE_DEMOTE		15
 #define TDH_MR_EXTEND			16
@@ -99,10 +100,22 @@
 
 enum tdx_tdcs_execution_control {
 	TD_TDCS_EXEC_TSC_OFFSET = 10,
+	TD_TDCS_EXEC_VM_CTLS	= 18,
 };
+
+union tdx_tdcs_exec_vm_ctls {
+	struct {
+		u64 ept_violation_on_l2sept	: 1;
+		u64 reserved2_63		: 61;
+	};
+	u64 full;
+};
+
+#define TDX_TDCS_EXEC_VM_CTLS_VALID_MASK	(1ULL)
 
 /* @field is any of enum tdx_tdcs_execution_control */
 #define TDCS_EXEC(field)		BUILD_TDX_FIELD(TD_CLASS_EXECUTION_CONTROLS, (field))
+#define TDCS_EXEC_NON_ARCH(field)	BUILD_TDX_FIELD_NON_ARCH(17, (field))
 
 /* @field is the VMCS field encoding */
 #define TDVPS_VMCS(field)		BUILD_TDX_FIELD(TDVPS_CLASS_VMCS, (field))

arch/x86/kvm/vmx/tdx.c
@@ -4193,12 +4193,30 @@ static int tdx_td_init(struct kvm *kvm, struct kvm_tdx_cmd *cmd)
 		kvm->arch.gfn_shared_mask = gpa_to_gfn(BIT_ULL(47));
 
 	if (cmd->flags != KVM_TDX_INIT_VM_F_POST_INIT) {
+		int i;
+
 		err = tdh_mng_init(kvm_tdx->tdr_pa, __pa(td_params), &out);
 		if (WARN_ON_ONCE(err)) {
 			pr_tdx_error(TDH_MNG_INIT, err, &out);
 			ret = -EIO;
 			goto out;
 		}
+
+		for (i = 0; i < kvm_tdx->num_l2_vms; i++) {
+			enum tdx_vm_index vm_index = index_to_tdx_vm_index(i);
+			union tdx_tdcs_exec_vm_ctls vm_ctls = { .full = 0 };
+
+			vm_ctls.ept_violation_on_l2sept = 1;
+			err = tdh_mng_wr(kvm_tdx->tdr_pa,
+					 TDCS_EXEC_NON_ARCH(TD_TDCS_EXEC_VM_CTLS + vm_index),
+					 vm_ctls.full, TDX_TDCS_EXEC_VM_CTLS_VALID_MASK, &out);
+			if (WARN_ON_ONCE(err)) {
+				pr_tdx_error(TDH_MNG_WR, err, &out);
+				ret = -EIO;
+				goto out;
+			}
+		}
+
 		tdx_td_post_init(kvm_tdx);
 	}
 

arch/x86/kvm/vmx/tdx_ops.h
@@ -342,6 +342,13 @@ static inline u64 tdh_mng_rd(hpa_t tdr, u64 field, struct tdx_module_output *out
 			    tdr, field, 0, 0, 0, 0, 0, 0, out);
 }
 
+static inline u64 tdh_mng_wr(hpa_t tdr, u64 field, u64 data, u64 mask,
+			     struct tdx_module_output *out)
+{
+	return kvm_seamcall(TDH_MNG_WR, tdr,
+			    field, data, mask, 0, 0, 0, 0, out);
+}
+
 static inline u64 tdh_mem_page_demote(hpa_t tdr, gpa_t gpa, int level, hpa_t page,
 				      struct tdx_module_output *out)
 {

commit fba675c298206c8167160346080b302e6b18cb26

KVM: Fix typo of defination tdx_tdcs_exec_vm_ctls
Fixes: 71bb594717d17 (KVM: TDX: config VM_CTLS for L2 VM)
Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>

arch/x86/kvm/vmx/tdx_arch.h
@@ -106,7 +106,7 @@ enum tdx_tdcs_execution_control {
 union tdx_tdcs_exec_vm_ctls {
 	struct {
 		u64 ept_violation_on_l2sept	: 1;
-		u64 reserved2_63		: 61;
+		u64 reserved1_63		: 63;
 	};
 	u64 full;
 };