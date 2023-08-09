d829f023b91f4830aecb6092bc7b3212b5f0142b
Wei Wang <wei.w.wang@intel.com>
2023-08-09 15:52:24 +0800

    kvm: mmu: api to pre-allocate all the private memory pages
    add an api, kvm_slot_prealloc_private_pages, to pre-allocate the guest's
    private memory pages, i.e. the memslots whose private_file field has been
    assigned to allocate pages for the guest private memory. the "nonleaf"
    parameter is for callers to specify if the leaf entries need to be
    pre-allocated, for example, if non_leaf=true, only the private page tables
    will be allocated and the last level leaf entries are kept empty.
    
    Here are two usages:
    1) improve the guest runtime performance by reducing the ept violation
       overhead during the guest runtime. this usually sets nonleaf=false to
       have the leaf pages allocated.
    2) improve the guest live migration performance by reducing the ept
       setup overhead during the live migration process. this usually sets
       nonleaf=true to have the leaf entries not added, and then in later
       migration process, they are added when the related leaf page is
       imported.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -1797,8 +1797,9 @@ int kvm_tdp_mmu_map(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 
 	raw_gfn = gpa_to_gfn(fault->addr);
 
-	if (is_error_noslot_pfn(fault->pfn) ||
-	    !kvm_pfn_to_refcounted_page(fault->pfn)) {
+	if (!fault->nonleaf &&
+	    (is_error_noslot_pfn(fault->pfn) ||
+	    !kvm_pfn_to_refcounted_page(fault->pfn))) {
 		if (is_private) {
 			rcu_read_unlock();
 			return -EFAULT;
@@ -1875,8 +1876,10 @@ int kvm_tdp_mmu_map(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault)
 	goto retry;
 
 map_target_level:
-	ret = tdp_mmu_map_handle_target_level(vcpu, fault, &iter);
-
+	if (!fault->nonleaf)
+		ret = tdp_mmu_map_handle_target_level(vcpu, fault, &iter);
+	else
+		ret = RET_PF_FIXED;
 retry:
 	rcu_read_unlock();
 	return ret;
