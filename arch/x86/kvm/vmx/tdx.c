intel/tdx

KVM: TDX: Ignore some error of TDH.MEM.PAGE_AUG
EPT violation can happen when page is pending and
TD.attribute.SEPT_VE_DISABLE=1.  TDX module doens't inject #VE and trigger
EPT_VIOLATION instead.  In such case, TDH.MEM.PAGE.AUG can issue on the
same Secure-EPT entry and the second call results in error.  Don't complain
such error and let vcpu retry.

Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kvm/vmx/tdx.c
@@ -1831,6 +1831,23 @@ static int tdx_sept_set_private_spte(struct kvm *kvm, gfn_t gfn,
 				.raw = out.rdx,
 			};
 
+			/*
+			 * TD.attribute.sept_ve_disable=1 and EPT violation on
+			 * pending page. Probably it's a race condition or a bug
+			 * for guest TD to access unaccepted region.  Let vcpu
+			 * retry with the expectation of a race condition so that
+			 * other vcpu would accept the page.
+			 */
+			if (level_state.level == tdx_level &&
+			    level_state.state == TDX_SEPT_PENDING &&
+			    entry.leaf && entry.hpa == hpa && entry.sve) {
+				tdx_unpin(kvm, gfn, pfn, level);
+				WARN_ON_ONCE(!(to_kvm_tdx(kvm)->attributes &
+					       BIT(28) /* =ATTR_SEPT_VE_DISABLE) */));
+				WARN_ON_ONCE(1);
+				return -EAGAIN;
+			}
+
 			/* Someone updated the entry to the same value. */
 			if (level_state.level == tdx_level &&
 			    level_state.state == TDX_SEPT_PRESENT &&

intel/td-partitioning

arch/x86/kvm/vmx/tdx.c
@@ -1548,6 +1548,23 @@ static int tdx_sept_set_private_spte(struct kvm *kvm, gfn_t gfn,
 				.raw = out.rdx,
 			};
 
+			/*
+			 * TD.attribute.sept_ve_disable=1 and EPT violation on
+			 * pending page. Probably it's a race condition or a bug
+			 * for guest TD to access unaccepted region.  Let vcpu
+			 * retry with the expectation of a race condition so that
+			 * other vcpu would accept the page.
+			 */
+			if (level_state.level == tdx_level &&
+			    level_state.state == TDX_SEPT_PENDING &&
+			    entry.leaf && entry.pfn == pfn && entry.sve) {
+				tdx_unpin(kvm, gfn, pfn, level);
+				WARN_ON_ONCE(!(to_kvm_tdx(kvm)->attributes &
+					       BIT(28) /* =ATTR_SEPT_VE_DISABLE) */));
+				WARN_ON_ONCE(1);
+				return -EAGAIN;
+			}
+
 			/* Someone updated the entry to the same value. */
 			if (level_state.level == tdx_level &&
 			    level_state.state == TDX_SEPT_PRESENT &&