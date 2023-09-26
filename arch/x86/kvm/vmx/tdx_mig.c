kmaloor/tdx

KVM: TDX_MIG: add tdx cmd KVM_TDX_MIG_EXPORT_STATE_IMMUTABLE
The KVM_TDX_MIG_EXPORT_STATE_IMMUTABLE command is used by the userspace
migration thread to request KVM to export the TD-scope immutable state.
The command is sent via kvm_device created per migration stream.

The TDH_EXPORT_STATE_IMMUTABLE seamcall is invoked to export the immutable
states into the migration stream's buffers (e.g. mbmd buffer, page list
buffers). The buffers have been mapped by the userspace thread, so on
success the exported data are directly accessible by userspace from the
mapped memory. The number of exported 4KB pages are returned to userspace
via the tdx_cmd's data field for userspace to understand the size of the
states data that need to be sent to destination (the buffer size could be
larger than the actual data size exported). If the seamcall is
interrupted, set the "resume" flag and resume the seamcall till done.

Signed-off-by: Wei Wang <wei.w.wang@intel.com>

arch/x86/kvm/vmx/tdx_mig.c
@@ -125,6 +125,15 @@ union tdx_mig_ept_info {
 	};
 };
 
+union tdx_mig_stream_info {
+	uint64_t val;
+	struct {
+		uint64_t index	: 16;
+		uint64_t rsvd	: 47;
+		uint64_t resume	: 1;
+	};
+};
+
 struct tdx_mig_stream {
 	uint16_t idx;
 	uint32_t buf_list_pages;
@@ -620,10 +629,60 @@ static int tdx_mig_stream_mmap(struct kvm_device *dev,
 	return 0;
 }
 
+static int tdx_mig_export_state_immutable(struct kvm_tdx *kvm_tdx,
+					  struct tdx_mig_stream *stream,
+					  uint64_t __user *data)
+{
+	struct tdx_mig_page_list *page_list = &stream->page_list;
+	union tdx_mig_stream_info stream_info = {.val = 0};
+	struct tdx_module_output out;
+	uint64_t err;
+
+	do {
+		err = tdh_export_state_immutable(kvm_tdx->tdr_pa,
+						 stream->mbmd.addr_and_size,
+						 page_list->info.val,
+						 stream_info.val,
+						 &out);
+		if (seamcall_masked_status(err) == TDX_INTERRUPTED_RESUMABLE)
+			stream_info.resume = 1;
+	} while (seamcall_masked_status(err) == TDX_INTERRUPTED_RESUMABLE);
+
+	if (err == TDX_SUCCESS) {
+		stream->idx = stream->mbmd.data->migs_index;
+		/* Tell userspace the num of exported 4KB pages */
+		if (copy_to_user(data, &out.rdx, sizeof(uint64_t)))
+			return -EFAULT;
+	} else {
+		pr_err("%s: failed, err=%llx\n", __func__, err);
+		return -EIO;
+	}
+
+	return 0;
+}
+
 static long tdx_mig_stream_ioctl(struct kvm_device *dev, unsigned int ioctl,
 				 unsigned long arg)
 {
-	return -ENXIO;
+	struct kvm_tdx *kvm_tdx = to_kvm_tdx(dev->kvm);
+	struct tdx_mig_stream *stream = dev->private;
+	void __user *argp = (void __user *)arg;
+	struct kvm_tdx_cmd tdx_cmd;
+	int r;
+
+	if (copy_from_user(&tdx_cmd, argp, sizeof(struct kvm_tdx_cmd)))
+		return -EFAULT;
+
+	switch (tdx_cmd.id) {
+	case KVM_TDX_MIG_EXPORT_STATE_IMMUTABLE:
+		r = tdx_mig_export_state_immutable(kvm_tdx, stream,
+					(uint64_t __user *)tdx_cmd.data);
+		break;
+	default:
+		r = -EINVAL;
+	}
+
+	return r;
 }
 
 static int tdx_mig_do_stream_create(struct kvm_tdx *kvm_tdx,

intel/td-partitioning

arch/x86/kvm/vmx/tdx_mig.c
@@ -125,6 +125,15 @@ union tdx_mig_ept_info {
 	};
 };
 
+union tdx_mig_stream_info {
+	uint64_t val;
+	struct {
+		uint64_t index	: 16;
+		uint64_t rsvd	: 47;
+		uint64_t resume	: 1;
+	};
+};
+
 struct tdx_mig_stream {
 	uint16_t idx;
 	uint32_t buf_list_pages;
@@ -621,10 +630,60 @@ static int tdx_mig_stream_mmap(struct kvm_device *dev,
 	return 0;
 }
 
+static int tdx_mig_export_state_immutable(struct kvm_tdx *kvm_tdx,
+					  struct tdx_mig_stream *stream,
+					  uint64_t __user *data)
+{
+	struct tdx_mig_page_list *page_list = &stream->page_list;
+	union tdx_mig_stream_info stream_info = {.val = 0};
+	struct tdx_module_args out;
+	uint64_t err;
+
+	do {
+		err = tdh_export_state_immutable(kvm_tdx->tdr_pa,
+						 stream->mbmd.addr_and_size,
+						 page_list->info.val,
+						 stream_info.val,
+						 &out);
+		if (seamcall_masked_status(err) == TDX_INTERRUPTED_RESUMABLE)
+			stream_info.resume = 1;
+	} while (seamcall_masked_status(err) == TDX_INTERRUPTED_RESUMABLE);
+
+	if (err == TDX_SUCCESS) {
+		stream->idx = stream->mbmd.data->migs_index;
+		/* Tell userspace the num of exported 4KB pages */
+		if (copy_to_user(data, &out.rdx, sizeof(uint64_t)))
+			return -EFAULT;
+	} else {
+		pr_err("%s: failed, err=%llx\n", __func__, err);
+		return -EIO;
+	}
+
+	return 0;
+}
+
 static long tdx_mig_stream_ioctl(struct kvm_device *dev, unsigned int ioctl,
 				 unsigned long arg)
 {
-	return -ENXIO;
+	struct kvm_tdx *kvm_tdx = to_kvm_tdx(dev->kvm);
+	struct tdx_mig_stream *stream = dev->private;
+	void __user *argp = (void __user *)arg;
+	struct kvm_tdx_cmd tdx_cmd;
+	int r;
+
+	if (copy_from_user(&tdx_cmd, argp, sizeof(struct kvm_tdx_cmd)))
+		return -EFAULT;
+
+	switch (tdx_cmd.id) {
+	case KVM_TDX_MIG_EXPORT_STATE_IMMUTABLE:
+		r = tdx_mig_export_state_immutable(kvm_tdx, stream,
+					(uint64_t __user *)tdx_cmd.data);
+		break;
+	default:
+		r = -EINVAL;
+	}
+
+	return r;
 }
 
 static int tdx_mig_do_stream_create(struct kvm_tdx *kvm_tdx,