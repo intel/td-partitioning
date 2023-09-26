intel/tdx

KVM: TDX: Introduce memory reading specific functions
2 memory type reading accessor, private and shared reading
functions are introduced. TDX limited 8 bytes as chunk to
reading guest private memory. kvm_read_guest_atomic() is
used to access the shared memory.

Signed-off-by: Yuan Yao <yuan.yao@intel.com>
Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

arch/x86/kvm/vmx/tdx_ops.h
@@ -109,6 +109,11 @@ static inline u64 tdh_vp_create(hpa_t tdr, hpa_t tdvpr)
 	return __seamcall(TDH_VP_CREATE, tdvpr, tdr, 0, 0, NULL);
 }
 
+static inline u64 tdh_mem_rd(hpa_t tdr, gpa_t addr, struct tdx_module_output *out)
+{
+	return __seamcall(TDH_MEM_RD, addr, tdr, 0, 0, out);
+}
+
 static inline u64 tdh_mng_rd(hpa_t tdr, u64 field, struct tdx_module_output *out)
 {
 	return __seamcall(TDH_MNG_RD, tdr, field, 0, 0, out);

intel/td-partitioning

arch/x86/kvm/vmx/tdx_ops.h
@@ -283,6 +283,11 @@ static inline u64 tdh_vp_create(hpa_t tdr, hpa_t tdvpr)
 	return r;
 }
 
+static inline u64 tdh_mem_rd(hpa_t tdr, gpa_t addr, struct tdx_module_args *out)
+{
+	return tdx_seamcall(TDH_MEM_RD, addr, tdr, 0, 0, out);
+}
+
 static inline u64 tdh_mng_rd(hpa_t tdr, u64 field, struct tdx_module_args *out)
 {
 	return tdx_seamcall(TDH_MNG_RD, tdr, field, 0, 0, out);